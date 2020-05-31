f3a58dac6ec19876c4fd5bccec44682fddd2d0d2
Danylo Mysak <danmysak@gmail.com>
2020-05-31 19:14:47 +0300

    Minor fixes

--- source/web/main.js
+++ source/web/main.js
@@ -11,13 +11,13 @@ if (document.readyState === 'loading') {
 function initialize() {
   const inputElement = document.querySelector('.input');
   const statsContainer = document.querySelector('.stats-container');
-  const navigationContainer = document.querySelector('.input-navigation');
   const overlay = document.querySelector('.input-overlay');
-  const settingCheckboxes = document.querySelectorAll('.settings-container input[type=checkbox][data-ignore-label]');
+  const navigationContainer = document.querySelector('.input-navigation');
   const navigationElements = [
     document.querySelector('.input-navigation-previous'),
     document.querySelector('.input-navigation-next')
   ];
+  const settingCheckboxes = document.querySelectorAll('.settings-container input[type=checkbox][data-ignore-label]');
   inputElement.focus(); // Setting the autofocus attribute doesn't seem to work in Firefox
   registerOverlay(overlay);
   attachObservers(inputElement, statsContainer, settingCheckboxes, navigationElements);
