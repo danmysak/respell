2b760dab12fc31106ff2f59b937a8ee9d7138dfe
Danylo Mysak <danmysak@gmail.com>
2020-06-04 00:24:19 +0300

    Addressing a Safari bug

--- source/web/normalizer.js
+++ source/web/normalizer.js
@@ -1,4 +1,4 @@
-import {cursorPlaceholder, serializeCursor, restoreCursor} from "./cursor.js";
+import {cursorPlaceholder, serializeCursor, restoreCursor, getRangeIfInside} from "./cursor.js";
 import {paragraphTag, tokenTag} from "./common-tags.js";
 
 function isTokenTag(node) {
@@ -70,12 +70,26 @@ function removeLineBreaks(inputElement) {
   }
 }
 
+function fixEmptyElementSelectionSafariBug(inputElement) {
+  // Working around a variation of this bug: https://bugs.webkit.org/show_bug.cgi?id=15256: for some reason, it seems
+  // possible to put selection inside an empty paragraph, yet creating a range within an empty token inside an empty
+  // paragraph moves the caret to the previous paragraph... This scenario is possible when the user presses enter to
+  // create a new paragraph when the current paragraph ends with a correction token.
+  const range = getRangeIfInside(inputElement);
+  if (range !== null && range.collapsed && range.startContainer.nodeType === Node.ELEMENT_NODE
+    && range.startContainer.tagName === tokenTag && range.startContainer.textContent.length === 0
+    && range.startContainer.parentElement.childNodes.length === 1) {
+    range.startContainer.remove();
+  }
+}
+
 export function normalize(inputElement, paragraphsToSkip) {
   if (!inputElement.hasChildNodes()) {
     const emptyParagraph = document.createElement(paragraphTag);
     emptyParagraph.textContent = cursorPlaceholder;
     inputElement.append(emptyParagraph);
   } else {
+    fixEmptyElementSelectionSafariBug(inputElement);
     serializeCursor(inputElement);
   }
   removeLineBreaks(inputElement); // Simply replacing line breaks with '\n' doesn't work well in Firefox (it treats
