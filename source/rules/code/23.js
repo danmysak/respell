10d2d063cbc0fae79a3c76fcbd9d6d39af93647d
Danylo Mysak <danmysak@gmail.com>
2020-06-15 08:56:11 +0300

    Fixes to the abbreviations rule

--- source/rules/code/23.js
+++ source/rules/code/23.js
@@ -14,7 +14,8 @@ registerWordRule(createMaskRule({
     description: 'Відповідно до § 23 правопису, після слова, що завершується на приголосний, перед словом, '
       + 'що починається на голосний, ставимо «в».'
   }, {
-    nextCallback: (token) => token.match(/^[АЕИІЛМНОРСУФ][А-ЯҐЄІЇ]+$/),
+    nextCallback: (token) => token.match(new RegExp(`^[АЕИІЛМНОРСУФ][${getConsonants(false).join('').toUpperCase()}]`))
+      && !["США"].includes(token),
     description: 'Відповідно до § 23 правопису, перед абревіатурою, у назві першої букви якої вимовляють голосний, '
       + 'слід ставити прийменник «в».'
   }]
