478930bbae5d068c4a684dfc072e37994b9241fe
Danylo Mysak <danmysak@gmail.com>
2020-06-18 22:06:35 +0300

    Period after quotes ending in a punctuation mark

---
+++ source/rules/code/164.js
@@ -0,0 +1,44 @@
+import {
+  Correction,
+  correctionTypes,
+  registerPunctuationRule,
+  canBeSentenceBoundary,
+  isWord,
+  isWhitespace,
+  isQuote,
+  isCapitalized,
+  getFirstLetter
+} from "../imports.js";
+
+const period = ".";
+
+function examineNext(chain) {
+  let level = 1;
+  while (true) {
+    const token = chain.getNextToken(level);
+    if (token === null) {
+      return true;
+    }
+    if (isWord(token)) {
+      return isCapitalized(getFirstLetter(token));
+    }
+    level++;
+  }
+}
+
+registerPunctuationRule((token, chain) => {
+  if (!isQuote(token)) {
+    return null;
+  }
+  const previousToken = chain.getPreviousToken(1, false);
+  const nextToken = chain.getNextToken(1, false);
+  if ((previousToken !== null && canBeSentenceBoundary(previousToken) && previousToken !== period)
+    && (nextToken === null || (isWhitespace(nextToken) && examineNext(chain)))) {
+    return new Correction(correctionTypes.MISTAKE, token + period,
+      'Відповідно до § 164 правопису, якщо фрагмент у лапках закінчує речення, після лапок потрібно поставити крапку '
+        + 'навіть у тому випадку, якщо в кінці фрагмента стоїть знак питання, знак оклику чи три крапки.'
+    );
+  } else {
+    return null;
+  }
+});
