52d80600836b462e3ec109d4cc4ae54ad0d6eff1
Danylo Mysak <danmysak@gmail.com>
2020-06-21 18:09:50 +0300

    Feminitives for nouns ending in 'олог'

--- website/source/data/feminitives.js
+++ website/source/data/feminitives.js
@@ -4,52 +4,53 @@ import {data as _0} from './feminitives/банник-банниця.js';
 import {data as _1} from './feminitives/ант-антка.js';
 import {data as _2} from './feminitives/бранець-бранка.js';
 import {data as _3} from './feminitives/читач-читачка.js';
-import {data as _4} from './feminitives/киянин-киянка.js';
-import {data as _5} from './feminitives/арієць-арійка.js';
-import {data as _6} from './feminitives/вояк-воячка.js';
-import {data as _7} from './feminitives/житель-жителька.js';
-import {data as _8} from './feminitives/бідар-бідарка.js';
-import {data as _9} from './feminitives/кумик-кумичка.js';
-import {data as _10} from './feminitives/буржуй-буржуйка.js';
-import {data as _11} from './feminitives/банщик-банщиця.js';
-import {data as _12} from './feminitives/обранець-обраниця.js';
-import {data as _13} from './feminitives/дояр-доярка.js';
-import {data as _14} from './feminitives/агулець-агулка.js';
-import {data as _15} from './feminitives/діяч-діячка.js';
-import {data as _16} from './feminitives/шалапутник-шалапутка.js';
-import {data as _17} from './feminitives/умілець-умілиця.js';
-import {data as _18} from './feminitives/біс-бісиця.js';
-import {data as _19} from './feminitives/витівник-витівниця.js';
-import {data as _20} from './feminitives/герцог-герцогиня.js';
+import {data as _4} from './feminitives/біолог-біологиня.js';
+import {data as _5} from './feminitives/киянин-киянка.js';
+import {data as _6} from './feminitives/арієць-арійка.js';
+import {data as _7} from './feminitives/вояк-воячка.js';
+import {data as _8} from './feminitives/житель-жителька.js';
+import {data as _9} from './feminitives/бідар-бідарка.js';
+import {data as _10} from './feminitives/кумик-кумичка.js';
+import {data as _11} from './feminitives/буржуй-буржуйка.js';
+import {data as _12} from './feminitives/банщик-банщиця.js';
+import {data as _13} from './feminitives/обранець-обраниця.js';
+import {data as _14} from './feminitives/дояр-доярка.js';
+import {data as _15} from './feminitives/агулець-агулка.js';
+import {data as _16} from './feminitives/діяч-діячка.js';
+import {data as _17} from './feminitives/шалапутник-шалапутка.js';
+import {data as _18} from './feminitives/умілець-умілиця.js';
+import {data as _19} from './feminitives/біс-бісиця.js';
+import {data as _20} from './feminitives/витівник-витівниця.js';
 import {data as _21} from './feminitives/поет-поетеса.js';
-import {data as _22} from './feminitives/циган-циганка.js';
-import {data as _23} from './feminitives/раб-рабиня.js';
-import {data as _24} from './feminitives/багатир-багатирка.js';
-import {data as _25} from './feminitives/монах-монашка.js';
-import {data as _26} from './feminitives/чех-чешка.js';
-import {data as _27} from './feminitives/кореєць-кореянка.js';
-import {data as _28} from './feminitives/вертихвіст-вертихвістка.js';
-import {data as _29} from './feminitives/учень-учениця.js';
-import {data as _30} from './feminitives/бойко-бойкиня.js';
-import {data as _31} from './feminitives/бог-богиня.js';
-import {data as _32} from './feminitives/критик-критикеса.js';
-import {data as _33} from './feminitives/голубок-голубочка.js';
-import {data as _34} from './feminitives/консьєрж-консьєржка.js';
-import {data as _35} from './feminitives/куркуль-куркулька.js';
-import {data as _36} from './feminitives/маляр-малярка.js';
-import {data as _37} from './feminitives/поганин-поганинка.js';
-import {data as _38} from './feminitives/угрин-угринка.js';
-import {data as _39} from './feminitives/шапсуг-шапсужка.js';
-import {data as _40} from './feminitives/жрець-жриця.js';
-import {data as _41} from './feminitives/боярин-бояриня.js';
-import {data as _42} from './feminitives/господар-господиня.js';
-import {data as _43} from './feminitives/государ-государиня.js';
-import {data as _44} from './feminitives/ґазда-ґаздиня.js';
-import {data as _45} from './feminitives/майстер-майстриня.js';
-import {data as _46} from './feminitives/монах-монахиня.js';
-import {data as _47} from './feminitives/цар-цариця.js';
-import {data as _48} from './feminitives/чернець-черниця.js';
-import {data as _49} from './feminitives/король-королева.js';
+import {data as _22} from './feminitives/герцог-герцогиня.js';
+import {data as _23} from './feminitives/циган-циганка.js';
+import {data as _24} from './feminitives/раб-рабиня.js';
+import {data as _25} from './feminitives/багатир-багатирка.js';
+import {data as _26} from './feminitives/монах-монашка.js';
+import {data as _27} from './feminitives/чех-чешка.js';
+import {data as _28} from './feminitives/кореєць-кореянка.js';
+import {data as _29} from './feminitives/вертихвіст-вертихвістка.js';
+import {data as _30} from './feminitives/учень-учениця.js';
+import {data as _31} from './feminitives/бойко-бойкиня.js';
+import {data as _32} from './feminitives/бог-богиня.js';
+import {data as _33} from './feminitives/критик-критикеса.js';
+import {data as _34} from './feminitives/голубок-голубочка.js';
+import {data as _35} from './feminitives/консьєрж-консьєржка.js';
+import {data as _36} from './feminitives/куркуль-куркулька.js';
+import {data as _37} from './feminitives/маляр-малярка.js';
+import {data as _38} from './feminitives/поганин-поганинка.js';
+import {data as _39} from './feminitives/угрин-угринка.js';
+import {data as _40} from './feminitives/шапсуг-шапсужка.js';
+import {data as _41} from './feminitives/жрець-жриця.js';
+import {data as _42} from './feminitives/боярин-бояриня.js';
+import {data as _43} from './feminitives/господар-господиня.js';
+import {data as _44} from './feminitives/государ-государиня.js';
+import {data as _45} from './feminitives/ґазда-ґаздиня.js';
+import {data as _46} from './feminitives/майстер-майстриня.js';
+import {data as _47} from './feminitives/монах-монахиня.js';
+import {data as _48} from './feminitives/цар-цариця.js';
+import {data as _49} from './feminitives/чернець-черниця.js';
+import {data as _50} from './feminitives/король-королева.js';
 
 export const feminitives = [
   _0,
@@ -101,5 +102,6 @@ export const feminitives = [
   _46,
   _47,
   _48,
-  _49
+  _49,
+  _50
 ];
