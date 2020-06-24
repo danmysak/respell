dc5135002bd1e5f506843439f1b5159174856006
Danylo Mysak <danmysak@gmail.com>
2020-06-24 23:36:04 +0300

    Typo

--- website/source/rules/code/130.js
+++ website/source/rules/code/130.js
@@ -6,19 +6,19 @@ registerWordRule(createMaskRule({
   ],
   replacement: "є",
   type: correctionTypes.MISTAKE,
-  description: 'Відподвідно до § 130 правопису, слово «дієз» (та похідні) слід писати з літерою «є».'
+  description: 'Відповідно до § 130 правопису, слово «дієз» (та похідні) слід писати з літерою «є».'
 }));
 
 registerWordRule(createMaskRule({
   matches: ["рі(елто)р*", "рі(елте)р*", "рі(єлте)р*"],
   replacement: "єлто",
   type: correctionTypes.MISTAKE,
-  description: 'Відподвідно до § 130 правопису, слово «рієлтор» пишеться з літерами «є» та «о».'
+  description: 'Відповідно до § 130 правопису, слово «рієлтор» пишеться з літерами «є» та «о».'
 }));
 
 registerWordRule(createMaskRule({
   matches: ["арі(е)л*", "дані(е)л*"],
   replacement: "є",
   type: correctionTypes.MISTAKE,
-  description: 'Відподвідно до § 130 правопису, імена «Арієль» та «Данієль» пишуться з літерою «є».'
+  description: 'Відповідно до § 130 правопису, імена «Арієль» та «Данієль» пишуться з літерою «є».'
 }));
